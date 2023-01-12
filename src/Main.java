3e0e69648d6292589bcaeb4afae52df64d1a66bd
Mwai <machariamwai@gmail.com>
2023-01-12 13:24:05 +0300

    Challenging a challenge

---
+++ src/Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Patient patient = new Patient("Brad", 28,
+                new Eye("Left Eye", "short sighted", "Blue", true),
+                new Eye("Right Eye", "Normal", "Blue", true),
+                new Heart("Heart", "Normal", 65),
+                new Stomach("Stomach", "Acidic", false),
+                new Skin("Skin", "Burnt", "White", 40));
+
+        System.out.println("Name: " + patient.getName());
+        System.out.println("Age: " + patient.getAge());
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean shouldFinish = false;
+
+        while (!shouldFinish) {
+            System.out.println("Choose an organ: " +
+                    "\n\t1. Left Eye" +
+                    "\n\t2. Right Eye"
+                    + "\n\t3. Heart"
+                    + "\n\t4. Stomach"
+                    + "\n\t5. Skin"
+                    + "\n\t6. Quit"
+            );
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    patient.getLeftEye().getDetails();
+                    if (patient.getLeftEye().isOpened()) {
+                        System.out.println("\t\t1. Close the Eye");
+                        if (scanner.nextInt() == 1) {
+                            patient.getLeftEye().close();
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        System.out.println("\t\t1. Open the Eye");
+                        if (scanner.nextInt() == 1) {
+                            patient.getLeftEye().open();
+                        } else {
+                            continue;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    patient.getRightEye().getDetails();
+                    if (patient.getRightEye().isOpened()) {
+                        System.out.println("\t\t1. Close the Eye");
+                        if (scanner.nextInt() == 1) {
+                            patient.getRightEye().close();
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        System.out.println("\t\t1. Open the Eye");
+                        if (scanner.nextInt() == 1) {
+                            patient.getRightEye().open();
+                        } else {
+                            continue;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    patient.getHeart().getDetails();
+                    System.out.println("\t\t1. Change the heart rate");
+                    if(scanner.nextInt() == 1){
+                        System.out.println("Enter the new Heart rate: ");
+                        int newHeartRate = scanner.nextInt();
+                        patient.getHeart().setRate(newHeartRate);
+                        System.out.println("Heart rate changed to: " + patient.getHeart().getRate());
+                    }else{
+                        continue;
+                    }
+                    break;
+                case 4:
+                    patient.getStomach().getDetails();
+                    System.out.println("\t\t1. Digest");
+                    if (scanner.nextInt() == 1){
+                        patient.getStomach().digest();
+                    }else{
+                        continue;
+                    }
+                    break;
+                case 5:
+                    patient.getSkin().getDetails();
+                    break;
+                default:
+                    shouldFinish = true;
+                    break;
+
+
+            }
+        }
+
+    }
+}
---
+++ src/Stomach.java
@@ -0,0 +1,31 @@
+public class Stomach extends Organ{
+    private boolean isEmpty;
+
+    public Stomach(String name, String medicalCondition, boolean isEmpty) {
+        super(name, medicalCondition);
+        this.isEmpty = isEmpty;
+    }
+
+    public void digest(){
+        System.out.println("Digesting begin");
+    }
+
+    @Override
+    public void getDetails() {
+        super.getDetails();
+        if(this.isEmpty()){
+            System.out.println("Need to be fed");
+        }
+        else{
+            System.out.println("Stomach full");
+        }
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
+    }
+}
